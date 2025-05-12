2083feaa19568d4e5f5d3d59f62a7e5fbe177e7e
0bserver <billnduka01@gmail.com>
2025-05-13 00:53:42 +0100

    Added specializations to combobox

--- src/main/java/com/skedlease/RegisterDoctorController.java
+++ src/main/java/com/skedlease/RegisterDoctorController.java
@@ -6,8 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
 import javafx.scene.Node;
 import okhttp3.Call;
+import okhttp3.Cookie;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import java.io.IOException;
 import org.json.JSONObject;
@@ -27,6 +30,89 @@ public class RegisterDoctorController {
     private TextField lastName;
     @FXML
     private TextField phoneNumber;
-    
-    
+    @FXML
+    private ComboBox<String> specializations;
+
+    @FXML
+    public void initialize()
+    {
+        specializations.getItems().addAll(getSpecialities());
+        
+    }
+
+    private List<String> getSpecialities()
+    {
+        List<String> specializations = new ArrayList<>(); // Declare outside the try block
+        try
+        {
+            String token = App.getCSRFToken();
+            OkHttpClient client = App.httpClient;
+
+            Request request = new Request.Builder()
+                .url("https://skedlease.onrender.com/specialities/")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Referer", "https://skedlease.onrender.com/")
+                .addHeader("X-CSRFToken", token)
+                .get()
+                .build();
+
+            Call call = client.newCall(request);
+            Response response = call.execute();
+            if(response.isSuccessful())
+            {
+                String responseBody = response.body().string();
+                JSONArray specialities = new JSONArray(responseBody);
+                
+                for (int i = 0; i < specialities.length(); i++) 
+                {
+                    JSONObject specialization = specialities.getJSONObject(i);
+                    String name = specialization.getString("name");
+                    specializations.add(name);
+                }
+            } 
+            else
+            {
+                System.out.println(response.code() + " " + response.body().string());
+            }
+        }
+        catch (IOException | org.json.JSONException ex) 
+        {
+            ex.printStackTrace();
+        }
+        return specializations; 
+    }
+
+    // private void register()
+    // {
+    //     String token = App.getCSRFToken();
+    //     OkHttpClient client = App.httpClient;
+
+    //     try {
+    //         JSONObject json = new JSONObject();
+    //         json.put("username", username.getText()); 
+    //         json.put("password", password.getText());  
+
+    //         MediaType JSON_TYPE = MediaType.get("application/json; charset=utf-8");
+    //         RequestBody body = RequestBody.create(json.toString(), JSON_TYPE);
+
+    //         Request request = new Request.Builder()
+    //             .url("https://skedlease.onrender.com/user/login/")
+    //             .addHeader("Content-Type", "application/json")
+    //             .addHeader("X-CSRFToken", token)
+    //             .addHeader("Referer", "https://skedlease.onrender.com/")
+    //             .post(body)
+    //             .build();
+
+    //         Call call = client.newCall(request);
+    //         Response response = call.execute();
+
+    //         if (response.isSuccessful()) 
+    //         {
+
+    //         }    
+
+    //     } catch (IOException | org.json.JSONException ex) {
+    //         ex.printStackTrace();
+    //     }
+    // }
 }
